4c1bd6694fb272d50532e388f3390d57939c705a
Matthieu Minier <matthieu.minier75@gmail.com>
2023-03-06 11:59:54 +1100

    fixed form being reset after a message is sent

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,10 @@
 class MessagesController < ApplicationController
+  
   def create
     @chatroom = Chatroom.find(params[:chatroom_id])
     @message = Message.new(message_params)
     @message.chatroom = @chatroom
     @message.user = current_user
-
     if @message.save
       ChatroomChannel.broadcast_to(
         @chatroom, 
@@ -16,11 +16,9 @@ class MessagesController < ApplicationController
     end
   end
 
-
   private
 
   def message_params
     params.require(:message).permit(:content)
   end
-  
 end
