ab0009d2b236a1703a1ae4ba968e4013165eed36
Matthieu Minier <matthieu.minier75@gmail.com>
2023-03-16 15:19:15 +1100

    added style to chat messages

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -4,7 +4,7 @@ import { createConsumer } from "@rails/actioncable"
 // Connects to data-controller="chatroom-subscription"
 export default class extends Controller {
   static targets = ["messages"]
-  static values = { chatroomId: Number }
+  static values = { chatroomId: Number, currentUserId: Number }
 
 
 
@@ -16,17 +16,37 @@ export default class extends Controller {
       {channel: "ChatroomChannel", id: this.chatroomIdValue },
       { received: (data) => {this.#insertMessage(data)}}
       )
-
-  }
+  };
   
   resetForm(event) {
     event.target.reset()
-  }
+  };
   
 
   #insertMessage(data) {
-    this.messagesTarget.insertAdjacentHTML("beforeend", data)
+    const currentUserIsSender = this.currentUserIdValue === data.sender_id
+    const messageElement = this.#buildMessageElement(currentUserIsSender, data.message)
+
+    this.messagesTarget.insertAdjacentHTML("beforeend", messageElement)
     this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
-  }
+  };
+
+  #buildMessageElement(currentUserIsSender, message) {
+    return `
+      <div class="message-row d-flex ${this.#justifyClass(currentUserIsSender)}">
+        <div class="${this.#userStyleClass(currentUserIsSender)}">
+          ${message}
+        </div>
+      </div>
+    `
+  };
+
+  #justifyClass(currentUserIsSender) {
+    return currentUserIsSender ? "justify-content-end" : "justify-content-start"
+  };
+  
+  #userStyleClass(currentUserIsSender) {
+    return currentUserIsSender ? "sender-style" : "receiver-style"
+  };
 
 }
